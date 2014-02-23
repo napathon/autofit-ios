0a64574b50aa3caf3f59dbea24bbd7cf77936595
Marko Kudjerski <marko@kudjerski.com>
2014-02-22 21:11:50 -0500

    - socket.io lib added. now setting up kiwi connection

---
+++ iBeaconDemo/KiwiController.h
@@ -0,0 +1,13 @@
+//
+//  KiwiController.h
+//  hackathon
+//
+//  Created by Marko Kudjerski on 2/22/2014.
+//  Copyright (c) 2014 Christopher Mann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KiwiController : NSObject
+
+@end
---
+++ iBeaconDemo/KiwiController.m
@@ -0,0 +1,20 @@
+//
+//  KiwiController.m
+//  hackathon
+//
+//  Created by Marko Kudjerski on 2/22/2014.
+//  Copyright (c) 2014 Christopher Mann. All rights reserved.
+//
+
+#import "KiwiController.h"
+
+@implementation KiwiController
+
+- (id)init {
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+@end
